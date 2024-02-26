8d040345eded4cf3256111442d2c36d21b6ebbd2
mayanktyagi45 <mayank.tyagi@comprotechnologies.com>
2024-02-26 10:12:18 +0530

    video-panel-ui: Create video panel page.

---
+++ frontend/src/components/SideNav.vue
@@ -0,0 +1,99 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  profile_img: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <aside class="sidenav">
+    <section class="user_details">
+        <!-- <img :src="profile_img" alt="">
+        <h3>{{ name }}</h3> -->
+        <img class="user_profile_img" src="../assets/images/itachi.jpg" alt="">
+        <h3 class="user_name">John Jacobs</h3>
+    </section>
+    <hr class="divider">
+
+    <section class="nav_list_details">
+        <ul class="nav_list">
+          <li class="nav_list_item">
+              <a class="nav_link" href="/">
+                <i class="fas fa-users nav_link_icon"></i> Participants
+              </a>
+          </li>
+          <li class="nav_list_item">
+            <a class="nav_link" href="/">
+                <i class="fas fa-comments nav_link_icon"></i> Chatting
+            </a>
+          </li>  
+        </ul>
+    </section>
+</aside>
+</template>
+
+<style>
+    .sidenav {
+        /* width: calc(30% - 50px); */
+        width: 20%;
+        background-color: #ffffff;
+        border-radius: 0.5rem;
+        padding: 10px;
+    }
+
+    .user_details {
+        display: flex;
+        align-self: flex-start;
+    }
+
+    .user_profile_img {
+        max-width: 3rem;
+        max-height: 3rem;
+        border-radius: 0.5rem;
+    }
+
+    .user_name {
+        font-weight: normal;
+        font-size: 1rem;
+        margin-left: 0.5rem;
+    }
+
+    .divider {
+        width: 100%;
+        margin: 0.5rem 0;
+    }
+
+    .nav_list_details {
+        margin-top: 2rem;
+    }
+
+    .nav_list {
+        list-style-type: none;
+        /* display: flex;
+        flex-direction: column; */
+        /* justify-content: start;
+        align-items: flex-start; */
+    }
+
+    .nav_list_item {
+        padding-bottom: 1rem; 
+    }
+
+    .nav_link {
+        color: black;
+        text-decoration: none;
+        font-weight: 700;
+        font-size: 1.05rem;
+    }
+
+    .nav_link_icon {
+        color: #8dc6ff;
+        padding-right: 0.5rem
+    }
+
+</style>
---
+++ frontend/src/components/VideoPanel.vue
@@ -0,0 +1,68 @@
+<script setup>
+    const toggleMute = ()=>{}
+    const toggleVideo = ()=>{}
+    const endCall = ()=>{}
+</script>
+
+<template>
+  <div class="video_panel">
+    <p>Live Video Chat Feed</p>
+
+    <div class="video_feed">
+        <!-- Here the video will be shown -->
+        <p>Here the video will be shown</p>
+    </div>
+    <div class="controls">
+      <button @click="toggleMute" class="control_button"><i class="fas fa-microphone"></i></button>
+      <button @click="endCall" class="control_button end_call"><i class="fas fa-phone-slash"></i></button>
+      <button @click="toggleVideo" class="control_button"><i class="fas fa-video"></i></button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .video_panel {
+        background: #ffffff;
+        margin-left: 1rem;
+        width: 50%;
+        border-radius: 0.5rem;
+        padding: 1rem;
+    }
+
+    .video_feed {
+        background: yellow;
+        border-radius: 0.5rem;
+        min-width: 100%;
+        min-height: 75%;
+    }
+
+    .controls {
+        margin-top: 1rem;
+        display: flex;
+        justify-content: center;
+    }   
+
+    .control_button {
+        background: #484848;
+        border: none;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 10px;
+        cursor: pointer;
+        color: #ffffff;
+        transition: background-color 0.3s ease, border-color 0.3s ease;
+    }
+
+    .control_button:hover {
+        background: #B0B0B0;
+        border-color: #bbb;
+    }
+
+    .end_call {
+        background: red;
+    }
+</style>
