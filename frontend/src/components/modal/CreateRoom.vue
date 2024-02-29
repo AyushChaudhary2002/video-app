137f7a590309a88acdf13e27f2bf1ec40a2fc185
mayanktyagi45 <mayank.tyagi@comprotechnologies.com>
2024-02-29 10:05:35 +0530

    join-room-api: Create join room api.

--- frontend/src/components/modal/CreateRoom.vue
+++ frontend/src/components/modal/CreateRoom.vue
@@ -1,3 +1,10 @@
+<template>
+    <form class="modal-form create-form" @submit.prevent="submitCreateForm">
+        <input class="form-input" type="text" v-model="UserName" placeholder="Enter UserName">
+        <input class="form-submit create-room-btn" type="submit" value="Create">
+    </form>
+</template>
+
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
@@ -16,17 +23,34 @@ const submitCreateForm = async () => {
             'clientToken': client.data.clientToken
         }
       });
-    // joinSession(appId, client.data.clientSessionId, client.data.clientToken);
-    
 }
 
 </script>
 
-<template>
-    <form class="modal-form create-form" @submit.prevent="submitCreateForm">
-        <input class="form-input" type="text" v-model="UserName" placeholder="Enter UserName">
-        <input class="form-submit" type="submit" value="Create">
-    </form>
-</template>
+<style scoped>
+.modal-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-<style scoped></style>
+.form-input {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #DDDDDD;
+  color: #A0A6A3;
+  font-family: "Roboto Mono", sans-serif;
+  box-shadow: 1px 5px 9px rgba(211, 211, 211, .7);
+}
+
+.create-room-btn {
+  margin-top: 10px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  background-color: #003A74;
+  color: white;
+  cursor: pointer;
+  box-shadow: 1px 5px 9px rgba(211, 211, 211, .9);
+}
+</style>
--- frontend/src/components/modal/JoinRoom.vue
+++ frontend/src/components/modal/JoinRoom.vue
@@ -1,19 +1,55 @@
-<script setup>
-import { ref } from 'vue'
-const UserName = ref('');
-const SessionId = ref('');
-
-const submitJoinForm = () => {
-    
-}
-</script>
-
 <template>
     <form class="modal-form join-form" @submit.prevent="submitJoinForm">
-        <input class="form-input" type="text" v-model="UserName" placeholder="Enter UserName">
-        <input class="form-input" type="text"  v-model="SessionId" placeholder="Enter SessionId">
-        <input class="form-submit" type="submit" value="Join">
+        <input class="form-input" name="username" type="text" v-model="UserName" placeholder="Enter UserName">
+        <input class="form-input" name="session_id" type="text" v-model="SessionId" placeholder="Enter SessionId">
+        <input class="form-submit join-room-btn" type="submit" value="Join">
     </form>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const UserName = ref('');
+const SessionId = ref('');
+
+const submitJoinForm = async() => {
+    try {
+        const response = await axios.post('http://localhost:3000/api/v1/join_room', {
+            session_id: SessionId.value,
+            user_name: UserName.value
+        });
+        console.log(response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+</script>
+
+<style scoped>
+.modal-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-input {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #DDDDDD;
+  color: #A0A6A3;
+  font-family: "Roboto Mono", sans-serif;
+  box-shadow: 1px 5px 9px rgba(211, 211, 211, .7);
+}
+
+.join-room-btn {
+  margin-top: 10px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  background-color: #003A74;
+  color: white;
+  cursor: pointer;
+  box-shadow: 1px 5px 9px rgba(211, 211, 211, .9);
+}
+</style>
