c0076b4487828daeae0a3d537e5d05f75b4ec034
AyushChaudhary2002 <ayush.chaudhary@comprotechnologies.com>
2024-03-18 10:51:39 +0530

    Intialize: pre-test functionality

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import CallPage from '../views/CallPage.vue'
+import PreTestPage from '../views/PreTestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: CallPage
+    },
+    {
+      path: '/pretest',
+      name: 'pretest',
+      component: PreTestPage
     }
   ]
 })
